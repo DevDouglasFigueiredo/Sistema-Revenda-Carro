29f7017e80ececceac36f54c0a33cd13e7ba07d7
Douglas Figueiredo <devdouglasfigueiredo@gmail.com>
2022-06-05 12:38:16 -0300

    update

--- js/validacao.js
+++ js/validacao.js
@@ -10,6 +10,22 @@ function validaCarro (carro) {
         erros.push('Insira o nome do fabricante por favor')
     }
 
+    if (carro.ano.length == 0){
+        erros.push('Insira o ano do carro por favor')
+    }
+
+    if (carro.motor.length == 0){
+        erros.push('Insira o a potencia do motor por favor')
+    }
+
+    if (carro.combustivel.length == 0){
+        erros.push('declare se o carro é flex, GNV, gasolina ou alcool, por favor')
+    }
+
+    if (carro.cambio.length == 0){
+        erros.push('Insira o tipo de cambio, manual ou automatico, por favor')
+    }
+
     return erros;
 
 }
